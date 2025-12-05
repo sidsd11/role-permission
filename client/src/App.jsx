e6fc6fd93e86b0a88eefe53a4a96ef5076bcf695
Sidsd110401 <sid.deshetti@gmail.com>
2025-12-05 14:04:28 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,6 @@ const App = () => {
                 <Route path='/login' element={<Login />} />
                 <Route path='/home' element={<Home />} />
                 <Route path='*' element={<Navigate to='/login' replace />} />
-                <Route path='*' element={<Navigate to='/login' replace />} />
             </Routes>
         </div>
     )
